76ff961c311369768eb99c39811fa0e14fefda8f
Denzel-Fulcrum <denzel@001fulcrum.com.au>
2026-03-22 03:04:09 +1100

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import './globals.css'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Our Wedding Hub',
+  description: '28 November 2026',
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head><meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /></head>
+      <body>{children}</body>
+    </html>
+  )
+}
